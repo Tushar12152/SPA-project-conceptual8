f2163def555c31a6dda6239cac83211017be15ea
Tushar12152 <tusharimran470@gmail.com>
2023-09-13 07:55:03 -0600

    done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,15 @@ import  PropTypes from "prop-types";
 const Card = ({card}) => {
     // console.log(card)
     const {name}=card
-    console.log(name)
+    // console.log(name)
     return (
        <div className="sticky top-2">
-         <div className="sticky top-2">
-             <h1 className= " sticky top-2 bg-gray-400 mt-1 p-2 rounded-lg text-center w-[50%] mx-auto ">{name}</h1>
+         
+         <div >
+             <h1 className= " bg-gray-400 mt-1 p-2 rounded-lg text-center w-[50%] mx-auto ">{name}</h1>
+            
+
+             
         </div>
        </div>
     );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,6 +6,8 @@ import Card from "../Card/Card";
 const Home = () => {
 const [allActors,SetAllActors]=useState([])
 const [cards,setCards]=useState([])
+const [count,setCount]=useState(0)
+const [remaining,setRemaining]=useState(0)
 
 useEffect(()=>{
      const loadData=async()=>{
@@ -17,11 +19,40 @@ useEffect(()=>{
 },[])
 // console.log(allActors)
 
-const handdleSelect=(name)=>{
+const handdleSelect=(card)=>{
     // console.log(name)
-    setCards([...cards,name])
+const isExist=cards.find(item=>item.id===card.id)
+// console.log(isExist)
+// isExist?alert('Already booked'):setCards([...cards,card])
+let count=card.salary
+if(isExist){
+   return alert('Already booked')
+}
+else{
+    cards.forEach(item=>{
+        count=count+item.salary
+        
+    })
+    
+    
+     
+    setCards([...cards,card])
+}
+
+
+const remaining=20000-count;
+remaining<0? alert('Your Balance is Short'):setRemaining(remaining)
+
+
+  if(count<20000){
+    setCount(count)
+  }
+  else{
+    return
+  } 
 
 }
+console.log(remaining)
 
     return (
         <div className="  py-52 bg-cover  ">
@@ -39,9 +70,19 @@ const handdleSelect=(name)=>{
               <div className="border-2  border-white w-2/6 ">
                   <h1 className="text-center mt-4 text-white text-xl  ">press Select button to shoe Name  in this box</h1>
                      {/* <Card  card={cards}></Card> */}
+                    <div className="flex">
+                    <h1 className= "bg-violet-600 text-white mt-1 p-2 mb-8 rounded-lg text-center  mx-auto ">Total Hired: {cards.length}</h1>
+                     <h1  className= "bg-violet-600 text-white mt-1 p-2 mb-8 rounded-lg text-center  mx-auto ">Total cost:{count} $</h1>
+                    </div>
+
+                    <div className="flex">
+                    <h1 className= "bg-blue-950 text-white mt-1 p-2 mb-8 rounded-lg text-center  mx-auto ">Budget:20,000 $</h1>
+                     <h1  className= "bg-blue-950 text-white mt-1 p-2 mb-8 rounded-lg text-center  mx-auto ">Current Balance:{remaining} $</h1>
+                    </div>
                      {
                         cards.map((card,idx)=><Card key={idx} card={card}></Card>)
                      }
+
               </div>
               </div>
         </div>
